f5950cae6bbed7cd91b9479aa782a4532ac10fcf
Mar Barrantes Cepas <mar.bc3@gmail.com>
2025-02-21 17:52:15 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.viewcode',  # Link to local code
     #'myst_parser',          # For using Markdown inside reST
     #'sphinx_design',        # To add buttons and cards
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
