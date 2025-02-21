ed7f0751665cad26ba92ce300bc5277c8890d42d
Mar Barrantes Cepas <mar.bc3@gmail.com>
2025-02-21 16:40:41 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -82,6 +82,9 @@ myst_heading_anchors = 4
 # a list of builtin themes.
 
 #html_theme = 'pydata_sphinx_theme' 
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = { 'style_nav_header_background': '#259595'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
