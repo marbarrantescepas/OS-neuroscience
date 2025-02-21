aafdc1bda652f291a0a5d59c4100ec32c08a57b1
Mar Barrantes Cepas <mar.bc3@gmail.com>
2025-02-21 16:30:33 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ myst_heading_anchors = 4
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'pydata_sphinx_theme' 
+#html_theme = 'pydata_sphinx_theme' 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
